cf6e80efe4a5621bd66e5284d6102b04c1d9f33c
Mishal <mishal.sharekh@gmail.com>
2021-02-07 20:33:35 +0300

    final

--- DocumentsArchiving.Web/App_Start/RouteConfig.cs
+++ DocumentsArchiving.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DocumentsArchiving.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Documents", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
