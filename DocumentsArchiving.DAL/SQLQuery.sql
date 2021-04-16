48a365bd7c3785966dd8e22741a308281aa94512
Mishal <mishal.sharekh@gmail.com>
2021-04-16 07:26:23 +0300

    fix info

---
+++ DocumentsArchiving.DAL/SQLQuery.sql
@@ -0,0 +1,41 @@
+﻿CREATE TABLE [dbo].[DocumentType](
+	[DocumentTypeId] [int] IDENTITY(1,1) NOT NULL,
+	[DocumentTypeDesc] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_DocumentTyp] PRIMARY KEY CLUSTERED 
+(
+	[DocumentTypeId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+
+CREATE TABLE [dbo].[Document](
+	[DocumentId] [int] IDENTITY(1,1) NOT NULL,
+	[Subject] [nvarchar](50) NOT NULL,
+	[DocumentTypeId] [int] NOT NULL,
+	[DocumentDate] [date] NOT NULL,
+	[SerialNumber] [nvarchar](50) NOT NULL,
+	[Details] [nvarchar](max) NULL,
+	[Path] [nvarchar](200) NOT NULL,
+ CONSTRAINT [PK_Document] PRIMARY KEY CLUSTERED 
+(
+	[DocumentId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Document]  WITH CHECK ADD  CONSTRAINT [FK_Document_DocumentType] FOREIGN KEY([DocumentTypeId])
+REFERENCES [dbo].[DocumentType] ([DocumentTypeId])
+GO
+ALTER TABLE [dbo].[Document] CHECK CONSTRAINT [FK_Document_DocumentType]
+GO
+
+CREATE TABLE [dbo].[Transaction](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[TransactionDate] [datetime] NOT NULL,
+	[TransInfo] [nvarchar](50) NOT NULL,
+	[DocumentId] [int] NOT NULL,
+ CONSTRAINT [PK_Transaction] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
